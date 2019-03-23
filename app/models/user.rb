edd75b13527609a73ced3e8c7753be55a48f079e
github username <github email address>
2019-03-23 17:18:47 -0400

    worked on user-friend associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
-  has_many :friendships
-  has_many :friends, -> { where(status: "accepted") }, through: :friendships
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
+  has_many :users, through: :inverse_friendships
+
+  #has_many :friends, -> { where(status: "accepted") }, through: :friendships
+
+  #Return all users who are connected to the given user via a accepted friendship or accepted inverse friendship.
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.status == "accepted" }
+    new_friends_array = friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status == "accepted" }
+    new_friends_array
+  end
+
 end
